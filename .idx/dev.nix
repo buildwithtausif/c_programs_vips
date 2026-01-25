262daa5eaaff349588e28f51a673bf4704bd0a03
Tausif Alam <devops.tausifalam@outlook.com>
2026-01-25 13:22:37 +0530

    Delete .idx directory

--- .idx/dev.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs, ... }: {
-  # Which nixpkgs channel to use.
-  channel = "stable-23.11"; # or "unstable"
-
-  # Use https://search.nixos.org/packages to find packages
-  packages = [
-    pkgs.gcc
-  ];
-}
